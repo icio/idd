8b57505bc47e0f1e2981d238b93ee46b893b0b60
Paul Scott <paul.scott@ravelin.com>
2017-08-07 16:19:16 +0100

    Derp

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	http.Handle("/", http.HandlerFunc(handler))
+	fmt.Println(http.ListenAndServe(":9001", nil))
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	// Ensure it's a GET request.
+	if req.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Redirect: Seed the ID.
+	if len(req.URL.Query()) == 0 {
+		q := req.URL.Query()
+		q.Set("id", strconv.FormatUint(rand.Uint64(), 36))
+
+		w.Header().Set("Location", req.URL.Path+"?"+q.Encode())
+		w.WriteHeader(http.StatusMovedPermanently)
+		return
+	}
+
+	// Write the ID as a script.
+	w.Header().Set("Content-Type", "application/javascript")
+	for h, vs := range req.URL.Query() {
+		c, _ := json.Marshal(h + "=" + vs[0])
+		fmt.Fprintf(w, `document.cookie = %s;`, c)
+	}
+}
